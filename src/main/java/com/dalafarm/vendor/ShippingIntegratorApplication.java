b64e6f00c67246a3d3f428616af1151cd503045d
Le Tan Vu <vu.letan@bosch-si.com>
2017-08-06 20:49:07 +0800

    Renaming/Minor changes in text

--- src/main/java/com/dalafarm/vendor/ShippingIntegratorApplication.java
+++ src/main/java/com/dalafarm/vendor/ShippingIntegratorApplication.java
@@ -7,6 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ShippingIntegratorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(GhnApplication.class, args);
+		SpringApplication.run(ShippingIntegratorApplication.class, args);
 	}
 }
