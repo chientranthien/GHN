a9c26dab3bc968d0388e8dbe28d202c386944952
Le Tan Vu <vu.letan@bosch-si.com>
2017-08-07 21:59:38 +0800

    Use profile test for tests

--- src/test/java/com/dalafarm/vendor/OrderRepositoryTests.java
+++ src/test/java/com/dalafarm/vendor/OrderRepositoryTests.java
@@ -7,9 +7,11 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.ActiveProfiles;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class OrderRepositoryTests {
 
     @Autowired
