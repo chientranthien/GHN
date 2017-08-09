5751faac3f0e9991c28c4c44694aeb081af6e19f
Le Tan Vu <vu.letan@bosch-si.com>
2017-08-09 21:11:56 +0800

    Allow CORS

--- src/main/java/com/dalafarm/vendor/controller/LocationController.java
+++ src/main/java/com/dalafarm/vendor/controller/LocationController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 /**
  * Created by chien on 8/6/17.
@@ -23,11 +24,13 @@ public class LocationController {
     @Autowired
     DistrictRepository districtRepository;
 
+    @CrossOrigin(origins = {"https://dalafarm.com.vn","http://preview.dalafarm.com.vn","http://localhost:1313"})
     @RequestMapping(value = "provinces",method = RequestMethod.GET)
     Iterable<Province> getAll() {
         return provinceRepository.findAll();
     }
 
+    @CrossOrigin(origins = {"https://dalafarm.com.vn","http://preview.dalafarm.com.vn","http://localhost:1313"})
     @RequestMapping(value = "province/{provinceId}/districts", method = RequestMethod.GET)
     Iterable<District> getByProvinceId(@PathVariable String provinceId) {
         return districtRepository.findByProvinceId(provinceId);
