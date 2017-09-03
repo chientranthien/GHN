0261fc2662e0608d583d7924806960bfd64e73e0
Le Tan Vu <imleeu@gmail.com>
2017-09-03 22:02:52 +0800

    Display product list for each order

--- src/main/java/com/dalafarm/vendor/service/OrderService.java
+++ src/main/java/com/dalafarm/vendor/service/OrderService.java
@@ -1,9 +1,17 @@
 package com.dalafarm.vendor.service;
 
 import com.dalafarm.vendor.model.Order;
+import com.dalafarm.vendor.model.OrderProduct;
+import com.dalafarm.vendor.model.Product;
 import com.dalafarm.vendor.repository.OrderRepository;
+import com.dalafarm.vendor.repository.ProductRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+import java.util.stream.StreamSupport;
 
 /**
  * Created by LeeU on 9/3/2017.
@@ -13,9 +21,22 @@ public class OrderService {
     @Autowired
     private OrderRepository orderRepository;
 
+    @Autowired
+    private ProductRepository productRepository;
+
     public Iterable<Order> getAllOrdersForFrontend(){
         Iterable<Order> orders = orderRepository.findAll();
-
-        return orders;
+        Iterable<Product> products = productRepository.findAll();
+        return StreamSupport.stream(orders.spliterator(), true).map(o -> {
+            List<OrderProduct> orderProductList = o.getOrderProducts();
+            o.setProducts(orderProductList.stream().map(op -> {
+                Product product = StreamSupport.stream(products.spliterator(), true)
+                        .filter(p -> op.getProductId().compareTo(p.getId()) == 0)
+                        .findFirst().orElse(null);
+                return product;
+            })
+                    .collect(Collectors.toList()));
+            return o;
+        }).collect(Collectors.toList());
     }
 }
