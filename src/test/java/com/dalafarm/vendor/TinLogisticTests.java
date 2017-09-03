bfd619f01679fca01bc0a9bf05fa49c38c3765ad
Le Tan Vu <imleeu@gmail.com>
2017-09-03 22:32:02 +0800

    Fix unit tests

--- src/test/java/com/dalafarm/vendor/TinLogisticTests.java
+++ src/test/java/com/dalafarm/vendor/TinLogisticTests.java
@@ -20,6 +20,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@ActiveProfiles("test")
 public class TinLogisticTests {
     @Autowired
     @Qualifier("tinLogistic")
